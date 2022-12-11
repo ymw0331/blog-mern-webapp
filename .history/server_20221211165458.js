980028c702220b6b076110e33dd9d8b14cdbbff8
Wayne Yong <ymw0331@gmail.com>
2022-12-11 17:23:37 +0800

    setup mongoDB and add it to express

---
+++ .history/server_20221211165458.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const { MongoClient } = require("mongodb")
+const PORT = process.env.PORT || 8000
+
+// Initialize middleware, body parser not needed as it is built in middleware
+// function of express, parse icoming JSON payload
+app.use(express.json({ extended: false }));
+
+
+app.get('/api/articles/:name', async (req, res) => {
+  const articleName = req.params.name;
+  const client = await MongoClient.connect('mongodb://localhost:27017')
+  const db = client.db('mernblog')
+  const articleInfo = db.collection("articles").findOne({ name: articleName })
+  client.close()
+})
+
+// just a test route
+// app.get('/', (req, res) => res.send('hello world'))
+// app.post('/', (req, res) => res.send(`Hello ${req.body.name}, thank you for using postman`))
+// app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`))
+app.post('/api/articles/:name/add-comments', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articleInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articleInfo[articleName]);
+})
+
+
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));

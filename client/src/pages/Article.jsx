bb3fa762da51369dff7d47ba923c0262e1c0da57
Wayne Yong <ymw0331@gmail.com>
2022-12-11 18:28:11 +0800

    complete both front and back

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articleContent from './article-content';
 // Pages
 import NotFound from './NotFound';
 // Components
 import Articles from '../components/Articles'
+import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 
 const Article = () => {
   const { name } = useParams(); //name refered to App.js article route /:name
   const article = articleContent.find((article) => article.name === name);
+  const [articleInfo, setArticleInfo] = useState({ comments: [] })
+
+  //run every time component load/ mounted
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`)
+      const body = await result.json()
+      console.log(body)
+      setArticleInfo(body)
+    }
+    fetchData()
+  }, [name])
+
   if (!article) return <NotFound />;
   const otherArticles = articleContent.filter(article => article.name !== name)
   return (
@@ -22,7 +37,8 @@ const Article = () => {
           {paragraph}
         </p>
       ))}
-
+      <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
       <h1 className='sm:text-2xl text-xl font-bold my-4 text-gray-900'>
         Other Articles
       </h1>
@@ -30,7 +46,6 @@ const Article = () => {
       <div className='flex flex-wrap -m-4'>
         <Articles articles={otherArticles} />
       </div>
-
 
     </>
   );
